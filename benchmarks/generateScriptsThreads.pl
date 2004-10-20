939badfda06a2315405bde5227111d2e044b45c3
Sears Russell <sears@cs.berkeley.edu>
2004-10-20 23:02:16 +0000

    New benchmark. :)

---
+++ benchmarks/generateScriptsThreads.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+
+## This perl script generates the input to timer.pl, which in turn
+## generates the input to plotting.pl, which generates performance
+## graphs. :)
+
+open(LOGICAL_THREADS,     ">LOGICAL_THREADS.script"    );
+
+for(my $i = 1; $i <= 20; $i += .5) {
+    my $total = 100000;
+
+    my $thread_count = $i;
+
+    my $insert_count = $total / $i;
+
+    print LOGICAL_THREADS         "./logicalMultThreaded     $thread_count $insert_count\n";
+
+} 
+
+close(LOGICAL_THREADS);
