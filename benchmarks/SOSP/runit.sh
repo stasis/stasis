58aaf2acb855e8c324d12b34a8e08884573c66c0
Sears Russell <sears@cs.berkeley.edu>
2005-03-22 20:37:30 +0000

    Script that runs all of the benchmarks in order...

---
+++ benchmarks/SOSP/runit.sh
@@ -0,0 +1,5 @@
+cat BULK_LOAD.script     | timer.pl > BULK_LOAD.out
+cat BULK_LOAD_RAW.script | timer.pl > BULK_LOAD_RAW.out
+cat LLADD_HASH_TPS.script| timer.pl > LLADD_HASH_TPS.out
+cat TPS.script           | timer.pl > TPS.out
+cat SEDA.script          | timer.pl > SEDA.out
