03b5c12fdda57e36a662491a687bccb286ae4736
Sears Russell <sears@cs.berkeley.edu>
2006-05-24 02:26:54 +0000

    test file for linear hash

---
+++ test/lladd/check_lhtable.c
@@ -0,0 +1,123 @@
+/*---
+This software is copyrighted by the Regents of the University of
+California, and other parties. The following terms apply to all files
+associated with the software unless explicitly disclaimed in
+individual files.
+
+The authors hereby grant permission to use, copy, modify, distribute,
+and license this software and its documentation for any purpose,
+provided that existing copyright notices are retained in all copies
+and that this notice is included verbatim in any distributions. No
+written agreement, license, or royalty fee is required for any of the
+authorized uses. Modifications to this software may be copyrighted by
+their authors and need not follow the licensing terms described here,
+provided that the new terms are clearly indicated on the first page of
+each file where they apply.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ARISING OUT OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY
+DERIVATIVES THEREOF, EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND
+NON-INFRINGEMENT. THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, AND
+THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+GOVERNMENT USE: If you are acquiring this software on behalf of the
+U.S. government, the Government shall have only "Restricted Rights" in
+the software and related documentation as defined in the Federal
+Acquisition Regulations (FARs) in Clause 52.227.19 (c) (2). If you are
+acquiring the software on behalf of the Department of Defense, the
+software shall be classified as "Commercial Computer Software" and the
+Government shall have only "Restricted Rights" as defined in Clause
+252.227-7013 (c) (1) of DFARs. Notwithstanding the foregoing, the
+authors grant the U.S. Government and others acting in its behalf
+permission to use and distribute the software in accordance with the
+terms specified in this license.
+---*/
+
+#define _GNU_SOURCE
+#include <stdio.h>
+
+#include <config.h>
+#include <check.h>
+
+#include <lladd/lhtable.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../check_includes.h"
+/*
+#include <sys/time.h>
+#include <time.h>
+*/
+#define LOG_NAME   "check_lhtable.log"
+
+/**
+   @test 
+*/
+
+#define NUM_ENTRIES 10000
+
+START_TEST(lhtableTest)
+{
+  /*  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  srand(tv.tv_sec + tv.tv_usec);
+  */
+
+  char** keys = malloc(NUM_ENTRIES * sizeof(char*));
+  struct LH_ENTRY(table) * t = LH_ENTRY(create)(100);
+  for(long i = 0; i < NUM_ENTRIES; i++) { 
+    int keyLen = asprintf(&(keys[i]), "--> %ld <--\n", i);
+    assert(keyLen == strlen(keys[i]));
+    assert(!LH_ENTRY(find)(t, keys[i], strlen(keys[i])));
+    LH_ENTRY(insert)(t, keys[i], strlen(keys[i]), (void*)i);
+    assert((void*)i == LH_ENTRY(find)(t, keys[i], strlen(keys[i])));
+
+  }
+  
+  for(long i = 0; i < NUM_ENTRIES; i+=2) { 
+    char * key;
+    int keyLen = asprintf(&key, "--> %ld <--\n", i);
+    
+    assert((void*)i == LH_ENTRY(find)(t, key, strlen(key)));
+    LH_ENTRY(remove)(t, keys[i], strlen(keys[i]));
+    assert(!LH_ENTRY(find)(t, keys[i], strlen(keys[i])));
+    free(key);
+  }
+  LH_ENTRY(destroy)(t);
+  
+  for(int i = 0; i < NUM_ENTRIES; i++) { 
+    free(keys[i]);
+  }
+  free(keys);
+
+
+} END_TEST
+
+
+Suite * check_suite(void) {
+  Suite *s = suite_create("lhtable");
+  /* Begin a new test */
+  TCase *tc = tcase_create("lhtable");
+
+  /* Sub tests are added, one per line, here */
+
+  tcase_add_test(tc, lhtableTest);
+
+  /* --------------------------------------------- */
+  
+  tcase_add_checked_fixture(tc, setup, teardown);
+
+
+  suite_add_tcase(s, tc);
+  return s;
+}
+
+#include "../check_setup.h"
