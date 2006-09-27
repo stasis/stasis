c74afad7e374eba3d52e5d19e6442885e6e5baa8
Sears Russell <sears@cs.berkeley.edu>
2006-09-27 20:32:51 +0000

    Uses the public API instead of old internal stuff.

--- utilities/truncate_log.c
+++ utilities/truncate_log.c
@@ -1,13 +1,15 @@
 #include <lladd/transactional.h>
-#include "../src/lladd/logger/logWriter.h"
+#include <lladd/logger/logger2.h>
+#include <lladd/truncation.h>
 int main(void) {
   Tinit();
-  syncLog();
-  lsn_t trunc_to = flushedLSN();
+  //  syncLog();
+  //  lsn_t trunc_to = LogFlushedLSN();
+  truncateNow();
   Tdeinit();
   
-  openLogWriter();
-  truncateLog(trunc_to);
+  //  openLogWriter();
+  //  truncateLog(trunc_to);
 
   return compensation_error();
 }
