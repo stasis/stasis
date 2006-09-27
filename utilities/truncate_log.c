d98a8140b6a5010c6304440a0ba125a553267617
Sears Russell <sears@cs.berkeley.edu>
2006-09-27 20:44:21 +0000

    Cleaned up old comments...

--- utilities/truncate_log.c
+++ utilities/truncate_log.c
@@ -1,15 +1,9 @@
 #include <lladd/transactional.h>
-#include <lladd/logger/logger2.h>
 #include <lladd/truncation.h>
 int main(void) {
   Tinit();
-  //  syncLog();
-  //  lsn_t trunc_to = LogFlushedLSN();
   truncateNow();
   Tdeinit();
-  
-  //  openLogWriter();
-  //  truncateLog(trunc_to);
 
   return compensation_error();
 }
