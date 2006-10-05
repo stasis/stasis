a3e3ad24532b6c30202d438afd743c9f9c775aa4
Sears Russell <sears@cs.berkeley.edu>
2006-10-05 01:00:10 +0000

    Added a 20 minute timeout to prevent deadlocks and other problems from blocking the test.

--- test/lladd/check_bufferManager.c
+++ test/lladd/check_bufferManager.c
@@ -257,7 +257,7 @@ Suite * check_suite(void) {
   Suite *s = suite_create("bufferManager");
   /* Begin a new test */
   TCase *tc = tcase_create("multithreaded");
-  tcase_set_timeout(tc, 0); // disable timeouts
+  tcase_set_timeout(tc, 1200); // twenty minute timeout
   /* Sub tests are added, one per line, here */
   tcase_add_test(tc, pageSingleThreadTest); 
   tcase_add_test(tc, pageSingleThreadWriterTest);
