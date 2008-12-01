1cddd8ab8c41952ac1225dcb072b3a558c980ce5
Sears Russell <sears@cs.berkeley.edu>
2008-12-01 04:24:24 +0000

    remove ancient obsolete file

--- src/stasis/replacementPolicy.c
+++
@@ -1,9 +0,0 @@
-#include <config.h>
-#include <lladd/constants.h>
-#include <lladd/replacementPolicy.h>
-
-replacementPolicy * initReplacementPolicy(int impl) {
-  if(IMPL = REPLACEMENT_POLICY_LRU) { 
-    
-  }
-}
