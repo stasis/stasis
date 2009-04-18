f864f6f66ccca9c3cce13817b4726fb652d8f963
Sears Russell <sears@cs.berkeley.edu>
2009-04-18 04:58:51 +0000

    added installation instructions, more autoconf cleanups

--- clean.sh
+++ clean.sh
@@ -14,3 +14,5 @@ find . | perl -ne 'print if (/\/storefile.txt$/)' | xargs rm -f
 find . | perl -ne 'print if (/\/logfile.txt$/)' | xargs rm -f
 find . | perl -ne 'print if (/\/blob._file.txt$/)' | xargs rm -f
 rm -f test/gmon.out test/lladd/gmon.out
+rm -f m4/ltsugar.m4 m4/libtool.m4 m4/ltversion.m4 m4/lt~obsolete.m4 m4/ltoptions.m4
+rm -f configure.in
