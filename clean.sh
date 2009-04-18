62b692fd6dbcfa9d47b1ee698f4fec2b42df2f39
Sears Russell <sears@cs.berkeley.edu>
2009-04-18 05:36:17 +0000

    remove more autotools dribbles and other cruft

--- clean.sh
+++ clean.sh
@@ -14,5 +14,6 @@ find . | perl -ne 'print if (/\/storefile.txt$/)' | xargs rm -f
 find . | perl -ne 'print if (/\/logfile.txt$/)' | xargs rm -f
 find . | perl -ne 'print if (/\/blob._file.txt$/)' | xargs rm -f
 rm -f test/gmon.out test/lladd/gmon.out
-rm -f m4/ltsugar.m4 m4/libtool.m4 m4/ltversion.m4 m4/lt~obsolete.m4 m4/ltoptions.m4
 rm -f configure.in
+rm -f config.guess config.h config.log config.status config.sub install-sh ltmain.sh mkinstalldirs py-compile
+rm -f m4/ltsugar.m4 m4/libtool.m4 m4/ltversion.m4 m4/lt~obsolete.m4 m4/ltoptions.m4 missing stamp-h1
